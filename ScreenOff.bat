19252967c008a01714773b1bcb9b72e6919ae80a
Ervel <51764827+ErveI@users.noreply.github.com>
2023-04-13 20:28:37 +0300

    Delete ScreenOff.bat

--- ScreenOff.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell (Add-Type '[DllImport(\"user32.dll\")]^public static extern int PostMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::PostMessage(-1,0x0112,0xF170,2)
